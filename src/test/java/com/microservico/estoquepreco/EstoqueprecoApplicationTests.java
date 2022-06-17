43fe66b102a6a600d3222b26084e07cb8cada02f
macindex <machollanda@gmail.com>
2022-06-17 16:19:17 -0300

    first commit

---
+++ src/test/java/com/microservico/estoquepreco/EstoqueprecoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservico.estoquepreco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstoqueprecoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
