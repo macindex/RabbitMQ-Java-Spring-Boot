ba27584c3355b8e1412c3216174d0e4e25def9c6
macindex <machollanda@gmail.com>
2022-06-18 15:17:33 -0300

    criacao de filas

---
+++ src/main/java/com/microservico/estoquepreco/connections/RabbitMQConnection.java
@@ -0,0 +1,57 @@
+package com.microservico.estoquepreco.connections;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.stereotype.Component;
+
+import com.microservico.estoquepreco.contantes.RabbitmqContantes;
+
+@Component
+public class RabbitMQConnection {
+	
+	private static final String NOME_EXCHANGE = "amq.direct";
+	
+	private AmqpAdmin amqpAdmin;
+	
+	public RabbitMQConnection(AmqpAdmin amqpAdmin) {
+		this.amqpAdmin = amqpAdmin;
+	}
+	
+	private Queue fila(String nomeFila) {
+		return new Queue(nomeFila, true, false, false);
+	}
+	
+	private DirectExchange trocaDireta() {
+		return new DirectExchange(NOME_EXCHANGE);
+	}
+	
+	private Binding relacionamento(Queue fila, DirectExchange troca) {
+		return new Binding(fila.getName(), Binding.DestinationType.QUEUE, troca.getName(), fila.getName(), null);
+	}
+	
+	@PostConstruct
+	private void adiciona() {
+	Queue filaEstoque =	this.fila(RabbitmqContantes.FILA_ESTOQUE);
+	Queue filaPreco =	this.fila(RabbitmqContantes.FILA_PRECO);
+	
+	
+	DirectExchange troca = this.trocaDireta();
+	
+	Binding ligacaoEstoque = this.relacionamento(filaEstoque, troca);
+	Binding ligacaoPreco = this.relacionamento(filaPreco, troca);
+
+	// Criando as filas no RabbitMQ
+	
+	this.amqpAdmin.declareQueue(filaEstoque);
+	this.amqpAdmin.declareQueue(filaPreco);
+	
+	this.amqpAdmin.declareExchange(troca);
+	
+	this.amqpAdmin.declareBinding(ligacaoEstoque);
+	this.amqpAdmin.declareBinding(ligacaoPreco);
+	}
+}
---
+++ src/main/java/com/microservico/estoquepreco/contantes/RabbitmqContantes.java
@@ -0,0 +1,7 @@
+package com.microservico.estoquepreco.contantes;
+
+public class RabbitmqContantes {
+
+	public static final String FILA_ESTOQUE = "ESTOQUE";
+	public static final String FILA_PRECO = "PRECO";
+}
