a80aeaea7e9eeea17a040eea4dbcbdc8398ae31d
macindex <machollanda@gmail.com>
2022-06-20 15:45:11 -0300

    Enviando mensagens - Service implement

--- src/main/java/com/microservico/estoquepreco/controller/EstoqueController.java
+++ src/main/java/com/microservico/estoquepreco/controller/EstoqueController.java
@@ -3,18 +3,24 @@ package com.microservico.estoquepreco.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.microservico.estoquepreco.contantes.RabbitmqContantes;
 import com.microservico.estoquepreco.dto.EstoqueDTO;
+import com.microservico.estoquepreco.service.RabbitmqService;
 
 @RestController
-@RequestMapping(value="/estoque")
+@RequestMapping(value = "/estoque")
 public class EstoqueController {
 	@Autowired
-	
+	private RabbitmqService rabbitmqService;
+
+	@PutMapping
 	private ResponseEntity alteraEstoque(@RequestBody EstoqueDTO estoqueDTO) {
+		this.rabbitmqService.enviaMensagem(RabbitmqContantes.FILA_ESTOQUE, estoqueDTO);
 		return new ResponseEntity(HttpStatus.OK);
 	}
 }
--- src/main/java/com/microservico/estoquepreco/controller/PrecoController.java
+++ src/main/java/com/microservico/estoquepreco/controller/PrecoController.java
@@ -1,18 +1,27 @@
 package com.microservico.estoquepreco.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.microservico.estoquepreco.contantes.RabbitmqContantes;
 import com.microservico.estoquepreco.dto.PrecoDTO;
+import com.microservico.estoquepreco.service.RabbitmqService;
 
 @RestController
 @RequestMapping(value="/preco")
 public class PrecoController {
 	
+	@Autowired
+	private RabbitmqService rabbitmqService;
+	
+	@PutMapping
 	private ResponseEntity alteraPreco(@RequestBody PrecoDTO precoDTO) {
+		this.rabbitmqService.enviaMensagem(RabbitmqContantes.FILA_PRECO, precoDTO);
 		return new ResponseEntity(HttpStatus.OK);
 	}
 }
