b06f34f9b3f6be5726cea53bd42ee9fd6d6c976f
macindex <machollanda@gmail.com>
2022-06-23 11:11:25 -0300

    ObjectMapper in rabbitmqService

--- src/main/java/com/microservico/estoquepreco/service/RabbitmqService.java
+++ src/main/java/com/microservico/estoquepreco/service/RabbitmqService.java
@@ -4,13 +4,25 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 @Service
 public class RabbitmqService {
-	
+
 	@Autowired
 	private RabbitTemplate rabbitTemplate;
-	
+
+	// ObjectMapper - Jackson: Serializa objeto java em json deserializa objetos
+	// json em objetos java
+	@Autowired
+	private ObjectMapper objectMapper;
+
 	public void enviaMensagem(String nomeFila, Object mensagem) {
-		this.rabbitTemplate.convertAndSend(nomeFila, mensagem);
+		try {
+		String mensagemJson = this.objectMapper.writeValueAsString(mensagem);
+		this.rabbitTemplate.convertAndSend(nomeFila, mensagemJson);
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
